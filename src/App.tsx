8e07dc9bf52caa64bce3aa1ff8dfe9e7d8a39586
martindzida <martin.d14@email.cz>
2022-10-25 11:26:15 +0200

    Article header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import ArticleHeader from './ArticleHeader';
 import Comment from './Comment';
 import ReplyNotification from './ReplyNotification';
 
@@ -45,18 +46,13 @@ function App() {
     }
   };
 
+  const date = new Date();
+
   return (
     <div className='w-full h-full flex flex-col justify-center items-center bg-amber-400 p-16'>
       <h1 className='text-6xl font-bold text-slate-700 text-center p-20'>Article Of The Year!</h1>
       <div className='bg-slate-600 text-white font-mono font-medium rounded-lg p-20'>
-        <p className='text-lg pb-6 flex flex-col'>
-          <span>
-            Author: <span className='font-bold'>Lorem</span>
-          </span>
-          <span>
-            Created: <span className='font-bold'>10. 10. 2022</span>
-          </span>
-        </p>
+        <ArticleHeader author='Lorem' createdAt={date} />
         <article>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reiciendis quam totam quas? Tempore dolorem facere soluta mollitia at nisi
           aliquam optio laudantium praesentium omnis est corporis aliquid, commodi quo reprehenderit cum aut velit asperiores labore vitae!
@@ -73,7 +69,7 @@ function App() {
         <h2 className='text-4xl text-slate-700 font-semibold p-8 border-b-2 border-slate-700 border-dashed m-8'>Comments</h2>
         <form onSubmit={handleSubmit} className='flex flex-col items-center bg-slate-600 rounded-lg  w-full p-10'>
           <textarea
-            className='resize-none w-full bg-slate-500 text-white font-mono text-lg caret-amber-400 rounded-lg p-6 focus:outline-none'
+            className='resize-none w-full bg-slate-500 text-white font-mono text-lg caret-amber-400 rounded-lg p-6 m-3 focus:outline-none'
             name='newComment'
             id='newComment'
             placeholder='Add new comment...'
@@ -84,7 +80,7 @@ function App() {
           {replyParentId !== null && <ReplyNotification parentCId={replyParentId} cancelReply={() => setReplyParentId(null)} />}
           {submitErr && <strong className='text-rose-500 font-mono text-bold text-lg mt-4'>Comment must not be empty string</strong>}
           <input
-            className='bg-slate-500 text-white font-mono text-lg font-semibold p-2 mt-6 rounded-lg cursor-pointer'
+            className='bg-slate-500 text-white font-mono text-lg font-semibold px-3 py-2 mt-6 rounded-lg cursor-pointer'
             type='submit'
             value='Add Comment'
           />
